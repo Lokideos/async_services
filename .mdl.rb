47e93452b4d74bf81b75770c88a6022bfd53b7fa
lokideos <archmage09@gmail.com>
2021-10-26 18:11:54 +0300

    Add mdl md linter

---
+++ .mdl.rb
@@ -0,0 +1,9 @@
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+
+all
+
+# Extend line length
+rule 'MD013', :line_length => 500
+
+# Allow to start file w/o header
+# exclude_rule 'MD041'

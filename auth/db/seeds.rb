1e9b2bd6f4561b22b90da60a398d6cb291f27e10
lokideos <archmage09@gmail.com>
2021-11-06 12:17:25 +0300

    Auth | Add db seeds

--- auth/db/seeds.rb
+++ auth/db/seeds.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 # This file should contain all the record creation needed to seed the database with its default values.
+
+users = []
+user_data = [{ name: 'Bob', email: 'bob@example.com', role: 'admin', password: 'givemeatoken' },
+             { name: 'Eva', email: 'eva@example.com', role: 'manager', password: 'givemeatoken' },
+             { name: 'Alice', email: 'alice@example.com', role: 'developer',
+               password: 'givemeatoken' }]
+user_data.each do |user|
+  users << User.create(user)
+end
+
+# Create user sessions
+session_data = [{ user: users[0] }, { user: users[1] }]
+session_data.each do |session|
+  UserSession.create(session)
+end

e63a31dec455380a8ab72a2940e9d800f0aa25a8
lokideos <archmage09@gmail.com>
2021-11-06 18:19:51 +0300

    Tasks | Fix code style

--- tasks/db/seeds.rb
+++ tasks/db/seeds.rb
@@ -8,19 +8,19 @@ time = Time.now
 user_data = [
   [SecureRandom.uuid, 'admin', time, time],
   [SecureRandom.uuid, 'manager', time, time],
-  [SecureRandom.uuid, 'developer', time, time]
+  [SecureRandom.uuid, 'developer', time, time],
 ]
 
 User.import(%i(gid role created_at updated_at), user_data)
 
 sessions_data = [
-  [SecureRandom.uuid, User.first&.id, time, time]
+  [SecureRandom.uuid, User.first&.id, time, time],
 ]
 
 UserSession.import(%i(gid user_id created_at updated_at), sessions_data)
 
 task_data = [
-  ['Add Title', 'Add Title to the page', 'in_progress', SecureRandom.uuid, User.last&.id, time, time]
+  ['Add Title', 'Add Title to the page', 'in_progress', SecureRandom.uuid, User.last&.id, time, time],
 ]
 
-Task.import(%i(title description status gid user_id created_at updated_at), sessions_data)
+Task.import(%i(title description status gid user_id created_at updated_at), task_data)

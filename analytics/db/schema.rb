6fa1e2c97b5e148645c044b8806eb37c3fbd6e41
lokideos <archmage09@gmail.com>
2021-11-15 04:53:50 +0200

    Analytics | Add balance to users

--- analytics/db/schema.rb
+++ analytics/db/schema.rb
@@ -10,6 +10,7 @@ Sequel.migration do
       primary_key :id, :type=>:Bignum
       column :gid, "uuid", :null=>false
       column :role, "roles_enum", :null=>false
+      column :balance, "numeric", :null=>false
       column :created_at, "timestamp(6) without time zone", :null=>false
       column :updated_at, "timestamp(6) without time zone", :null=>false
     end
